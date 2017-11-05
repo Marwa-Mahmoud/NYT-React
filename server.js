0c6622671257986a9f7785968748da4b40a3a936
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-11-04 19:54:36 -0700

    adding env.node for production on heroku

--- server.js
+++ server.js
@@ -23,16 +23,13 @@ mongoose.connect(
 
 
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.use(routes);
 
-// Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
